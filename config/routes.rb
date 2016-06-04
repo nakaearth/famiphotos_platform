e6c1a2619cac6221b750d7a4fb671ea63dbae96f
nakamura shinichirou <naka5313@gmail.com>
2016-06-05 08:46:34 +0900

    コミットもれ

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 FamiphotosPlatform::Engine.routes.draw do
+  # ユーザ認証関連
+  get "/:provider/login"  => "sessions#new"
+  get "/logout" => "sessions#destroy"
+  get "/auth/:provider/callback" => "sessions#create" unless Rails.env.development?
+  post "/auth/:provider/callback" => "sessions#create" if Rails.env.development?
+  get "/auth/failure" => "sessions#failuer"
+
 end
