969e0a12d6ab5a7d2be996479bec473d864637e3
nakamura shinichirou <naka5313@gmail.com>
2016-07-08 23:54:59 +0900

    追加

--- app/models/famiphotos_platform/famiphoto_photo_base.rb
+++ app/models/famiphotos_platform/famiphoto_photo_base.rb
@@ -2,6 +2,6 @@ module FamiphotosPlatform
   class FamiphotoPhotoBase < FamiphotoBase
     self.abstract_class = true
 
-    establish_connection(:famiphoto_photo_db))
+    establish_connection("famiphotos_photo_#{RAILS_ENV['ENV']}".to_sym)
   end
 end
